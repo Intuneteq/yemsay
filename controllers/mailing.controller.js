b71dd5c6f34e23419aa5c45fd6eea81ed16c9117
Tobi Olanitori <tobiolanitori@gmail.com>
2023-03-08 06:59:52 +0100

    feat: contactus and appointment apis Implemented

--- controllers/mailing.controller.js
+++ controllers/mailing.controller.js
@@ -11,7 +11,7 @@ const {
 
 //helpers
 const { allTrue } = require("../lib/payload");
-const { sendContactUsMail } = require("../lib/mailingList")
+const { sendContactUsMail, sendAppointmentMail } = require("../lib/mailingList");
 
 const handleContactUs = handleAsync(async (req, res) => {
   const { name, email, phoneNumber, message } = req.body;
@@ -19,10 +19,54 @@ const handleContactUs = handleAsync(async (req, res) => {
   const payload = allTrue(name, email, phoneNumber, message);
   if (!payload) throw createApiError("Payload Incomplete", 422);
 
-  const {error} = await sendContactUsMail(name, email, phoneNumber, message);
-  if(error) throw createApiError('email not sent', 500);
+  const { error } = await sendContactUsMail({
+    name,
+    email,
+    phoneNumber,
+    message,
+  });
+  if (error) throw createApiError("email not sent", 500);
 
-  res.status(250).json(handleResponse({ message: "email sent" }));
+  res.status(200).json(handleResponse({ message: "email sent" }));
 });
 
-module.exports = { handleContactUs };
+const handleBookAppointment = handleAsync(async (req, res) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    location,
+    inspectionDate,
+    inspectionTime,
+    message,
+  } = req.body;
+
+  const payload = allTrue(
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    location,
+    inspectionDate,
+    inspectionTime,
+    message
+  );
+  if (!payload) throw createApiError("Payload Incomplete", 422);
+
+  const { error } = await sendAppointmentMail({
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+    location,
+    inspectionDate,
+    inspectionTime,
+    message
+  });
+  if (error) throw createApiError("email not sent", 500);
+
+  res.status(200).json(handleResponse({ message: "email sent" }));
+});
+
+module.exports = { handleContactUs, handleBookAppointment };
--- routes/mailing.router.js
+++ routes/mailing.router.js
@@ -4,12 +4,13 @@ const router = express.Router();
 //import middleware
 
 //import controller
-const { handleContactUs } = require("../controllers/mailing.controller");
+const { handleContactUs, handleBookAppointment } = require("../controllers/mailing.controller");
 
 //get req
 
 //post req
 router.post("/contact-us", handleContactUs);
+router.post("/appointment", handleBookAppointment);
 
 //put or patch req
 

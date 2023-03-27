ffdbecdd18cb25105a13fcdc30e19561026bc109
Tobi Olanitori <tobiolanitori@gmail.com>
2023-03-27 11:50:29 +0100

    chore: uninstalled nodemailer-express-handlebars

---
+++ utils/mailingAlt.js
@@ -0,0 +1,20 @@
+const nodemailer = require('nodemailer');
+
+
+
+// Set up the Nodemailer transporter
+let transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    auth: {
+      user: process.env.AUTH_EMAIL,
+      pass: process.env.AUTH_PASS,
+    },
+  });
+
+
+
+const verifyTransPorter = async () => await transporter.verify();
+
+const sendMail = async (mailOptions) => await transporter.sendMail(mailOptions)
+
+module.exports = { verifyTransPorter, sendMail };

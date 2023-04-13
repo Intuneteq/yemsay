ef3e7b1f02d9af68660ecf6700113dcd55112d3f
Tobi Olanitori <tobiolanitori@gmail.com>
2023-04-13 02:43:05 +0100

    fix: fixed search query

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -16,32 +16,32 @@ const handleSearchQuery = handleAsync(async (req, res) => {
   const { location, property, propertyType, averagePrice } = req.query;
 
   /**
-    * @param property = propertyType in the property model;
-    * @param propertyType = features in the property model;
-    * @param location = location in Property model;
-    * @param averagePrice is derived and expected to match Price in property model
-  */
+   * @param property = propertyType in the property model;
+   * @param propertyType = features in the property model;
+   * @param location = location in Property model;
+   * @param averagePrice is derived and expected to match Price in property model
+   */
+  
+  if (!property) throw createApiError("bad request", 400);
 
   // Replace all consecutive spaces with a single space and then trim any leading/trailing spaces.
-  const locationSearch = location.replace(/\s+/g, " ").trim();
+  const locationSearch = location && location.replace(/\s+/g, " ").trim();
   const propertyTypeSearch = propertyType
     ? propertyType.replace(/\s/g, "").split("").join("\\s*")
     : undefined;
 
   // Define search regex
   const locationRegex = new RegExp(`\\b${locationSearch}\\b`, "i"); // Match a word boundary (i.e., the beginning or end of a word)
-  const propertyRegex = new RegExp(property, "i");
   const propertyTypeRegex = new RegExp(propertyTypeSearch, "i");
 
   // Build the query object
   const query = {
     $and: [
       { propertyStatus: "listed" },
-      { propertyType: { $regex: propertyRegex } },
       {
         $or: [
           { location: { $regex: locationRegex } },
-          { features: { $elemMatch: { $regex: propertyTypeRegex } } },
+          { features: propertyType && { $in: [propertyTypeRegex] } },
           {
             price: averagePrice && {
               $gte: averagePrice - averagePrice * 0.5,
@@ -54,10 +54,12 @@ const handleSearchQuery = handleAsync(async (req, res) => {
   };
 
   // Query DB with defined query
-  const foundProperties = await Properties.find(query);
+  const foundProperties = await Properties.find(query)
+    .where("propertyType")
+    .equals(property);
 
   // Format response
-  const response = foundProperties.map(prop => prop.miniFormat());
+  const response = foundProperties.map((prop) => prop.miniFormat());
 
   res.status(200).json(handleResponse(response));
 });

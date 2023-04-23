a3f60e676a833a3cfe9b844bfdcb4f3d8861c84a
Tobi Olanitori <tobiolanitori@gmail.com>
2023-04-23 20:57:57 +0100

    feat: changed email to yemsay's email address

--- utils/mailing.js
+++ utils/mailing.js
@@ -3,6 +3,7 @@ const nodemailer = require("nodemailer");
 // Set up the Nodemailer transporter
 let transporter = nodemailer.createTransport({
   host: "smtp-mail.outlook.com",
+  // service: "gmail",
   auth: {
     user: process.env.AUTH_EMAIL,
     pass: process.env.AUTH_PASS,
